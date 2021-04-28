d071d67b5f9dd68173247362bea74270451e4a8d
marco-amorim <marcoamorim95@hotmail.com>
2021-04-28 11:39:27 -0300

    dynamic auto wiring with @primary

--- src/main/java/com/spring/intro/springintro/BubbleSortAlgorithm.java
+++ src/main/java/com/spring/intro/springintro/BubbleSortAlgorithm.java
@@ -1,8 +1,10 @@
 package com.spring.intro.springintro;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class BubbleSortAlgorithm implements SortAlgorithm {
     public int[] sort(int[] numbers) {
         // Logic for Bubble Sort
--- src/main/java/com/spring/intro/springintro/QuickSortAlgorithm.java
+++ src/main/java/com/spring/intro/springintro/QuickSortAlgorithm.java
@@ -1,5 +1,8 @@
 package com.spring.intro.springintro;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class QuickSortAlgorithm implements SortAlgorithm {
     public int[] sort(int[] numbers) {
         // Logic for Quick Sort
